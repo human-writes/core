5984adc253babbb72b55e5e7d4328570608dbaa1
David Blanchard <dpjb@hotmail.fr>
2023-06-04 20:30:17 +0200

    Fix package links

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,7 @@
-declare module "@human-writes/core";
+import Decomposer from "../src/decomposer";
+import Writer from "../src/writer";
+import WriterComponent from "../src/writer-component";
+
+declare module "@human-writes/core" {
+    export = { Writer, WriterComponent, Decomposer };
+}
